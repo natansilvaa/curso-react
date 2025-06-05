2e62e65ae66b34980dbe5e8d13b74d78c9017dd7
natansilvaa <natan.picanco24@gmail.com>
2025-06-05 15:22:20 -0300

    aprendi a adicionar as img

--- projeto1/src/titulo.jsx
+++ projeto1/src/titulo.jsx
@@ -1,9 +1,12 @@
 function Titulo(){
 
     let nome = "Natan silva"
+    let urlImg = "https://devsagaz.com.br/content/images/size/w960/2023/05/Ekran-Resmi-2019-11-18-18.08.13.png"
+
     return (
         <div>
            <h1>oi eu sou {nome}</h1>
+           <img src={urlImg} />
            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, et provident sint aliquam aut, maxime omnis exercitationem, culpa porro dolorem explicabo modi dicta aliquid. Autem sed itaque rerum vitae asperiores?</p> 
            
         </div>

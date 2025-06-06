7e53c55df15f07b1977e1a4e1aab0882c1811502
natansilvaa <natan.picanco24@gmail.com>
2025-06-05 22:56:12 -0300

    1

--- projeto1/src/titulo.jsx
+++ projeto1/src/titulo.jsx
@@ -8,8 +8,7 @@ function Titulo(){
            <h1>oi eu sou {nome}</h1>
            <img width={100}  src={urlImg} />
            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, et provident sint aliquam aut, maxime omnis exercitationem, culpa porro dolorem explicabo modi dicta aliquid. Autem sed itaque rerum vitae asperiores?</p> 
-           <h2> <button>
-            clique </button></h2>
+           
            
         </div>
     ) 
